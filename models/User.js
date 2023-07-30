2e8fd3d40e4bb79647e66a1edea4d2cb4abad463
sai-charan-pushpak <60253094+p-v-sai-sri-charan@users.noreply.github.com>
2023-07-30 23:30:38 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,17 +1,21 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
 const userSchema = mongoose.Schema({
-    name: { type: String, required: false , default: "user"},
-    phone: { type: String, required: [true, "Please provide a valid Phone number"], unique: [true, "Phone number already exists"] },
-    email: { type: String, required: false, unique: [true, "Email already exists"], sparse: true },
-    otp: { type: String, required: false },
-    role: { type: String, required: false, default: "user" },
-    isAdmin: { type: Boolean, required: false, default: false },
-    otpExpires: { type: Date, required: false },
-    otpTries: { type: Number, required: false, default: 0 },
-    otpTriesExpires: { type: Date, required: false },
-
+  name: { type: String, required: false, default: 'user' },
+  phone: { type: String, required: [true, 'Please provide a valid Phone number'], unique: [true, 'Phone number already exists'] },
+  email: { type: String, required: false, unique: false, sparse: true }, // Set unique: false for the email field
+  otp: { type: String, required: false },
+  role: { type: String, required: false, default: 'user' },
+  isAdmin: { type: Boolean, required: false, default: false },
+  otpExpires: { type: Date, required: false },
+  otpTries: { type: Number, required: false, default: 0 },
+  otpTriesExpires: { type: Date, required: false },
 });
-const User = mongoose.models.user || mongoose.model("user", userSchema);
+
+// Manually create indexes
+userSchema.index({ email: 1 }, { unique: true, sparse: true });
+userSchema.index({ phone: 1 }, { unique: true });
+
+const User = mongoose.models.user || mongoose.model('user', userSchema);
 
 export default User;
