1c770c95aead539018886975aa10988658024e6d
sai-charan-pushpak <60253094+p-v-sai-sri-charan@users.noreply.github.com>
2023-07-30 23:17:52 +0530

    Update app.js

--- app.js
+++ app.js
@@ -15,12 +15,12 @@ dotenv.config();
 app.use(cors({ origin: 'https://master.d15nmek1sd12f8.amplifyapp.com', credentials: true }));
 
 //access-alllow-origin
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  next();
-});
+// app.use((req, res, next) => {
+//   res.setHeader('Access-Control-Allow-Origin', '*');
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+//   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//   next();
+// });
 
 // middleware
 app.use(bodyParser.json());
