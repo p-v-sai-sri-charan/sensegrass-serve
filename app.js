6650a91e673d507741c805d48f42517ac51a7345
p-v-sai-sri-charan <60253094+p-v-sai-sri-charan@users.noreply.github.com>
2023-07-30 12:47:16 +0530

    Update app.js

--- app.js
+++ app.js
@@ -32,6 +32,11 @@ app.use(
     optionsSuccessStatus: 200,
   })
 ); 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "https://master.d5wbnpaa0ldw8.amplifyapp.com"); 
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 
   // middleware
